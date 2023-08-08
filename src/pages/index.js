9bb87cf2edff76b1e100176d1f8f573ae708f9f7
Lee <lee.neufeld@ganjagurus.ca>
2023-08-08 14:14:17 -0600

    Completed Home and Who we are pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,10 @@
 import React from "react";
 import Layout from "../components/Layout";
-import {
-  Box,
-  Card,
-  CardMedia,
-  Divider,
-  Grid,
-  IconButton,
-  Paper,
-  Typography,
-} from "@mui/material";
+import { Card, CardMedia, Grid, Link, Typography } from "@mui/material";
 import styled from "@emotion/styled";
 import BlackLogo from "../images/AlderwoodConsultingLogoBlack.png";
 import Banff from "../images/Banff.jpg";
-import { ConnectWithoutContact } from "@mui/icons-material";
+import { ConnectWithoutContact, Handshake } from "@mui/icons-material";
 
 const StyledLogo = styled.img`
   padding: 25px;
@@ -23,118 +14,150 @@ const StyledLogo = styled.img`
 export default function Index() {
   return (
     <Layout>
-      <Box>
-        <Grid container spacing={0} direction="column" alignItems="center">
-          <Grid item xs={12}>
-            <StyledLogo src={BlackLogo} alt="Black Logo" />
-          </Grid>
-          <Grid item xs={12} paddingBottom={5}>
-            <Typography fontFamily="cursive">
-              Your Insurance & Risk Management Specialists
-            </Typography>
-          </Grid>
-          <Grid item>
-            <img
-              src={Banff}
-              alt="Banff Pic"
-              style={{
-                maxWidth: "100%",
-              }}
-            />
-          </Grid>
-          <Grid item>
-            <Typography
-              sx={{ padding: 5, maxWidth: "1500px" }}
-              textAlign="center"
-              alignSelf="center"
-            >
-              Insurance is a lot like oxygen – everyone needs it, very few
-              people understand how it works, and the lack of it can take your
-              life away. The financial safety net that hovers under nearly every
-              business, home, and vehicle in the developed world is an
-              interesting concept; hovering below you like a mysterious fog, it
-              can feel like a lot of cost for nothing – until you need it to
-              break your fall, and find out for the first time what quality of
-              net you’ve been paying for all these years. But what if it didn’t
-              have to be that way? What if you could understand that net you
-              were paying for, and the best ways to use it… or better yet, how
-              to keep from falling in the first place?
-            </Typography>
-          </Grid>
-          <Grid item sx={{ marginBottom: 5 }}>
-            <Card
-              sx={{
-                padding: 5,
-                mx: 5,
-                border: "2px solid black",
-                backgroundColor: "#D9D9D9",
-                maxWidth: "1500px",
-              }}
-              variant="outlined"
-            >
-              <Typography textAlign="center">
-                Please note that Rod and Danielle don’t sell or promote any type
-                of insurance product. That is what brokers are for – and they’re
-                great at it. Nor are they affiliated with, or working for, any
-                insurance company. Alderwood’s focus is two-fold; partnering
-                with companies and individuals to offer risk management and
-                claims advocacy services normally reserved for large
-                corporations, and offering those same people an engaging and
-                insightful look into how to be a better, smarter insurance
-                customer through educational presentations.
-              </Typography>
-            </Card>
-          </Grid>
-          <Grid item>
-            <Card
-              sx={{
-                maxWidth: "1500px",
-
-                marginLeft: 5,
-                marginRight: 5,
-              }}
-            >
-              <CardMedia component="img" image={Banff} alt="Banff Pic" />
-            </Card>
-          </Grid>
-
-          <Grid item>
-            <Typography
-              sx={{ padding: 5, maxWidth: "1500px" }}
-              textAlign="center"
-              alignSelf="center"
-            >
-              After many years and lots of experience in insurance and risk
-              management (and a lot of phone calls and questions at dinner
-              parties), Rod and Danielle Ahlskog had come to understand
-              something very important – that almost nobody understood insurance
-              and risk management, and how to make it work for them. Even most
-              insurance professionals tend to stay in one lane throughout their
-              career; selling insurance, adjusting claims, working for the
-              insurance companies themselves, or doing risk management. Very few
-              people had done it all, and so those who understood how all the
-              parts fit together in this complex machine were rare finds.
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Typography
-              sx={{ padding: 3, maxWidth: "1500px" }}
-              textAlign="center"
-              alignSelf="center"
-            >
-              Big corporations have dedicated risk managers and insurance
-              consultants on staff to handle this very issue – but what about
-              the average business owner, or consultant, or family? Alderwood
-              Consulting was founded to answer that question, bringing
-              specialized education and decades of experience out of the
-              boardroom and into the hands of everyday people as they work to
-              build a better future for their businesses and families. Because
-              knowledge and support power success, and should be within reach
-              for anyone willing to seek them out.
-            </Typography>
-          </Grid>
+      <Grid
+        container
+        direction="row"
+        alignItems="center"
+        justifyContent="center"
+        spacing={2}
+        minHeight={`calc(100vh - 200px)`}
+      >
+        <Grid item xs={12} sx={{ textAlign: "center" }}>
+          <StyledLogo src={BlackLogo} alt="Black Logo" />
         </Grid>
-      </Box>
+        <Grid item xs={12} sx={{ textAlign: "center" }} paddingBottom={5}>
+          <Typography fontFamily="cursive">
+            Your Insurance & Risk Management Specialists
+          </Typography>
+        </Grid>
+        <Grid item>
+          <img
+            src={Banff}
+            alt="Banff Pic"
+            style={{
+              maxWidth: "100%",
+            }}
+          />
+        </Grid>
+        <Grid item>
+          <Typography
+            sx={{ padding: 5, maxWidth: "1500px", color: "#1E1E1E" }}
+            textAlign="center"
+            variant="subtitle2"
+            fontWeight="800"
+          >
+            Insurance is a lot like oxygen – everyone needs it, very few people
+            understand how it works, and the lack of it can take your life away.
+            The financial safety net that hovers under nearly every business,
+            home, and vehicle in the developed world is an interesting concept;
+            hovering below you like a mysterious fog, it can feel like a lot of
+            cost for nothing – until you need it to break your fall, and find
+            out for the first time what quality of net you’ve been paying for
+            all these years. But what if it didn’t have to be that way? What if
+            you could understand that net you were paying for, and the best ways
+            to use it… or better yet, how to keep from falling in the first
+            place?
+          </Typography>
+        </Grid>
+        <Grid item sx={{ marginBottom: 5 }}>
+          <Card
+            sx={{
+              padding: 5,
+              mx: 5,
+              border: "2px solid black",
+              backgroundColor: "#D9D9D9",
+              maxWidth: "1500px",
+            }}
+            variant="outlined"
+          >
+            <Typography textAlign="center" variant="subtitle2" fontWeight="800">
+              Please note that Rod and Danielle don’t sell or promote any type
+              of insurance product. That is what brokers are for – and they’re
+              great at it. Nor are they affiliated with, or working for, any
+              insurance company. Alderwood’s focus is two-fold; partnering with
+              companies and individuals to offer risk management and claims
+              advocacy services normally reserved for large corporations, and
+              offering those same people an engaging and insightful look into
+              how to be a better, smarter insurance customer through educational
+              presentations.
+            </Typography>
+          </Card>
+        </Grid>
+        <Grid item>
+          <Card
+            sx={{
+              maxWidth: "1500px",
+
+              marginLeft: 5,
+              marginRight: 5,
+            }}
+          >
+            <CardMedia component="img" image={Banff} alt="Banff Pic" />
+          </Card>
+        </Grid>
+
+        <Grid item>
+          <Typography
+            sx={{ padding: 5, maxWidth: "1500px" }}
+            textAlign="center"
+            variant="subtitle2"
+            fontWeight="800"
+          >
+            After many years and lots of experience in insurance and risk
+            management (and a lot of phone calls and questions at dinner
+            parties), Rod and Danielle Ahlskog had come to understand something
+            very important – that almost nobody understood insurance and risk
+            management, and how to make it work for them. Even most insurance
+            professionals tend to stay in one lane throughout their career;
+            selling insurance, adjusting claims, working for the insurance
+            companies themselves, or doing risk management. Very few people had
+            done it all, and so those who understood how all the parts fit
+            together in this complex machine were rare finds.
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Typography
+            sx={{ padding: 3, maxWidth: "1500px" }}
+            textAlign="center"
+            variant="subtitle2"
+            fontWeight="800"
+          >
+            Big corporations have dedicated risk managers and insurance
+            consultants on staff to handle this very issue – but what about the
+            average business owner, or consultant, or family? Alderwood
+            Consulting was founded to answer that question, bringing specialized
+            education and decades of experience out of the boardroom and into
+            the hands of everyday people as they work to build a better future
+            for their businesses and families. Because knowledge and support
+            power success, and should be within reach for anyone willing to seek
+            them out.
+          </Typography>
+        </Grid>
+        <Grid item xs={12} sm={3} sx={{ textAlign: "center" }} mb={3}>
+          <Link
+            underline="hover"
+            href="/SpeakingEngagements"
+            sx={{ color: "#1E1E1E" }}
+          >
+            <ConnectWithoutContact sx={{ width: "75px", height: "75px" }} />
+            <Typography variant="subtitle1" sx={{ fontWeight: "800" }}>
+              Speaking Engagements
+            </Typography>
+          </Link>
+        </Grid>
+        <Grid item xs={12} sm={3} sx={{ textAlign: "center" }} mb={3}>
+          <Link
+            underline="hover"
+            href="/ConsultingServices"
+            sx={{ color: "#1E1E1E" }}
+          >
+            <Handshake sx={{ width: "75px", height: "75px" }} />
+            <Typography variant="subtitle1" sx={{ fontWeight: "800" }}>
+              Consulting Services
+            </Typography>
+          </Link>
+        </Grid>
+      </Grid>
     </Layout>
   );
 }
--- src/pages/whoWeAre.js
+++ src/pages/whoWeAre.js
@@ -1,10 +1,166 @@
 import React from "react";
 import Layout from "../components/Layout";
+import { Grid, Typography, Paper, Divider, Button } from "@mui/material";
+import styled from "@emotion/styled";
+import CoupleStock from "../images/CoupleSunStock.jpg";
+import ManStock from "../images/ManStock.jpg";
+import WomanStock from "../images/WomanStock.jpg";
+import WhoWeAreLogo from "../images/WhoWeAreLogo.png";
 
 export default function WhoWeAre() {
+  const StyledLogo = styled.img`
+    padding: 25px;
+    max-width: 400px;
+  `;
   return (
     <Layout>
-      <h1>Who We Are</h1>
+      <Grid
+        container
+        direction="row"
+        alignItems="center"
+        justifyContent="center"
+        spacing={2}
+        minHeight={`calc(100vh - 200px)`}
+      >
+        <Grid item xs={12} sx={{ textAlign: "center" }}>
+          <StyledLogo src={WhoWeAreLogo} alt="Who We Are" />
+        </Grid>
+        <Grid item>
+          <img
+            src={CoupleStock}
+            alt="Couple"
+            style={{
+              maxWidth: "100%",
+            }}
+          />
+        </Grid>
+        <Grid item>
+          <Typography
+            sx={{ padding: 5, maxWidth: "1000px", color: "#1E1E1E" }}
+            textAlign="center"
+            variant="subtitle2"
+            fontWeight="800"
+          >
+            Rod and Danielle met at work nearly twenty years ago, and have been
+            a dynamic duo in their personal lives since 2007. For most of those
+            years, they’ve worked together as well; spending eight hours a day
+            at separate offices just felt like wasted time. So where you find
+            one, you’ll almost always find the other.
+          </Typography>
+        </Grid>
+        <Grid item textAlign="center">
+          <Paper
+            sx={{
+              maxWidth: "1000px",
+              textAlign: "center",
+              padding: 3,
+              backgroundColor: "#E1DFE1",
+              mr: 1,
+              ml: 1,
+            }}
+          >
+            <Grid
+              container
+              direction="row"
+              alignItems="center"
+              justifyContent="center"
+              spacing={2}
+            >
+              <Grid item xs={12} md={6} mt={5}>
+                <img
+                  src={WomanStock}
+                  alt="Woman Stock"
+                  style={{ maxWidth: "300px" }}
+                />
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                md={6}
+                mt={5}
+                textAlign="center"
+                pr={{ xs: 0, md: 5 }}
+              >
+                <Typography>
+                  Danielle Ahlskog grew up in Hinton, Alberta, juggling a pile
+                  of extracurriculars with part-time jobs that ran the gamut
+                  from waitressing to bookkeeping. After attending Grant
+                  MacEwan’s Insurance & Risk Management program, she landed a
+                  job in a small-town insurance sales agency, and the rest is
+                  history. Danielle has two decades of experience in the
+                  industry, with particular concentration on sales, claims, and
+                  risk management. For someone who once thought insurance would
+                  be boring, she’s had a lot of adventures – investigating theft
+                  rings and international smugglers, busting a “severely
+                  injured” fraudster literally doing cartwheels on social media,
+                  and selling insurance policies for everything from mopeds to
+                  multi-million dollar companies. Danielle loves to read
+                  everything that she can get her hands on, traveling to and
+                  learning about new places, and that she managed to translate
+                  being a nerdy little neurotic pointing out every danger and
+                  pitfall to the other kids into a living.
+                </Typography>
+              </Grid>
+
+              <Grid item xs={12} md={6} mt={5}>
+                <img
+                  src={ManStock}
+                  alt="Man Stock"
+                  style={{ maxWidth: "300px" }}
+                />
+              </Grid>
+              <Grid item xs={12} md={6} mt={5} pr={{ xs: 0, md: 5 }}>
+                <Typography>
+                  Rod Ahlskog was raised on a farm north of Wildwood, Alberta,
+                  and spent his early life doing everything from delivering
+                  furniture to grading roads and driving transport trucks. After
+                  finding his true calling in his twenties, Rod spent decades
+                  exploring every aspect of insurance and risk management; sales
+                  for over ten years, another fifteen in claims, and stints at
+                  both private and government insurance companies in between.
+                  Starting out as simple agent and rising through management
+                  roles to ownership of a private insurance claims investigation
+                  firm, he has extensive experience with business, personal,
+                  farm, automobile, transportation, oil and gas, and liability
+                  risks – from perspectives that encompass sales, claims, and
+                  underwriting. Rod has a Chartered Insurance Professional
+                  designation, a passion for helping the little guy succeed in a
+                  big-business world, and a craving for medium-rare steak and a
+                  nice scotch.
+                </Typography>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+        <Grid item container mt={3} mb={5}>
+          <Grid
+            container
+            direction="row"
+            alignItems="center"
+            justifyContent="center"
+            spacing={9}
+          >
+            <Grid item textAlign="center">
+              <Button
+                variant="contained"
+                href={`mailto:rod@alderwoodconsulting.ca`}
+                sx={{ backgroundColor: "#3E5294", width: "250px" }}
+              >
+                Contact Rod
+              </Button>
+            </Grid>
+            <Grid item textAlign="center">
+              <Button
+                variant="contained"
+                href={`mailto:danielle@alderwoodconsulting.ca`}
+                sx={{ backgroundColor: "#3E5294", width: "250px" }}
+              >
+                Contact Danielle
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     </Layout>
   );
 }
